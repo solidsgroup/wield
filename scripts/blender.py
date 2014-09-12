3c595e6689811427159fc7dce9715b3c4677520a
Brandon Runnels <brunnels@caltech.edu>
2014-09-12 14:28:54 -0600

    Added blender scripts to render interfaces and draw voxel crystal texture

---
+++ scripts/blender.py
@@ -0,0 +1,188 @@
+import bpy
+from numpy import sqrt,linalg
+
+#
+# USER DEFINED VARIABLE REGION
+#
+b  = 0.125;
+n1 = [.4875,.844];   
+n2 = [-.4875,.844];
+n3 = [.0,-0.25];    
+rep1 = 10;
+rep2 = 10;
+lenX = 12;
+lenY = 12;
+lenZ = 4;
+seperation = 2;
+translation = (.25,.25,1);
+
+#Make normal vectors 3D
+n1 = [n1[0],n1[1],sqrt(1-linalg.norm(n1)**2)];
+n2 = [n2[0],n2[1],sqrt(1-linalg.norm(n2)**2)];
+n3 = [n3[0],n3[1],sqrt(1-linalg.norm(n3)**2)];
+print(n1);
+print(n2);
+print(n3);
+
+def add(a,b):
+    return [a[0]+b[0],a[1]+b[1],a[2]+b[2]];
+def subtract(a,b):
+    return [a[0]-b[0],a[1]-b[1],a[2]-b[2]];
+def multiply(a,alpha):
+    return [alpha*a[0],alpha*a[1],alpha*a[2]];
+def getIntersectionPoint(n1,n2,b):
+    A = [[n1[0],n1[1]],[n2[0],n2[1]]];
+    B = [b,b];
+    X = linalg.solve(A,B);
+    return [X[0],X[1],0];
+def addFacet(i,j,k):
+    verts=[];
+    faces=[];
+    #Points of original simplex
+    e0 = [0,0,b];
+    e1 = getIntersectionPoint(n2,n3,b);
+    e2 = getIntersectionPoint(n3,n1,b);
+    e3 = getIntersectionPoint(n1,n2,b);
+    #Offset vectors
+    d1 = multiply(subtract(e2,e1),i);
+    d2 = multiply(subtract(e3,e1),j);
+    d3 = multiply(subtract(e1,e2),k);
+    # Original symplex facet
+    verts.append(add(d1,add(d2,add(d3,e0))));
+    verts.append(add(d1,add(d2,add(d3,e1))));
+    verts.append(add(d1,add(d2,add(d3,e2))));
+    verts.append(add(d1,add(d2,add(d3,e3))));
+    # Inverted Facet
+    e0 = [e1[0]+e2[0]-e0[0],e1[1]+e2[1]-e0[1],-b];
+    verts.append(add(d1,add(d2,add(d3,e0))));
+    e1 = add(e1,subtract(e2,e3));
+    verts.append(add(d1,add(d2,add(d3,e1))));
+    # Add all faces
+    faces.append((0,1,2));
+    faces.append((0,2,3));
+    faces.append((0,3,1));
+    faces.append((4,2,1));
+    faces.append((4,5,2));
+    faces.append((4,1,5));
+    #Use vertices and faces to create blender object
+    mesh = bpy.data.meshes.new("facet");
+    object = bpy.data.objects.new("facet",mesh);
+    object.location=bpy.context.scene.cursor_location;
+    bpy.context.scene.objects.link(object);
+    mesh.from_pydata(verts,[],faces);
+    mesh.update(calc_edges=True);
+
+def addCube(lx,ly,lz,name="cube"):
+    verts=[];
+    faces=[];
+    #20  64
+    #31  75
+    verts.append((+lx/2,+ly/2,-lz/2));
+    verts.append((+lx/2,-ly/2,-lz/2));
+    verts.append((-lx/2,+ly/2,-lz/2));
+    verts.append((-lx/2,-ly/2,-lz/2));
+    verts.append((+lx/2,+ly/2,+lz/2));
+    verts.append((+lx/2,-ly/2,+lz/2));
+    verts.append((-lx/2,+ly/2,+lz/2));
+    verts.append((-lx/2,-ly/2,+lz/2));
+    faces.append((0,1,3,2));
+    faces.append((1,0,4,5)); # Ok
+    faces.append((2,3,7,6)); # Ok
+    faces.append((5,4,6,7)); # Ok
+    faces.append((0,2,6,4));
+    faces.append((3,1,5,7));
+    #Use vertices and faces to create blender object
+    mesh = bpy.data.meshes.new(name);
+    object = bpy.data.objects.new(name,mesh);
+    object.location=bpy.context.scene.cursor_location;
+    bpy.context.scene.objects.link(object);
+    mesh.from_pydata(verts,[],faces);
+    mesh.update(calc_edges=True);
+
+def allSelect(value=True,type='MESH'):
+    scene = bpy.context.scene;
+    for ob in scene.objects:
+        if ob.type==type:
+            ob.select=value;
+def select(name):
+    allSelect(False);
+    scene = bpy.context.scene;
+    for ob in scene.objects:
+        if ob.name==name:
+            ob.select=True;
+def selectActive(name):
+    select(name);
+    bpy.context.scene.objects.active = bpy.data.objects[name];
+def delete(name):
+    allSelect(False);
+    selectActive(name);
+    bpy.ops.object.delete();    
+    
+
+#Clear the scene
+if (bpy.context.scene.objects.active):
+    bpy.ops.object.mode_set(mode='OBJECT');
+scene = bpy.context.scene
+scene.cursor_location = (0,0,0);
+allSelect(True,type='MESH');
+allSelect(True,type='LAMP');
+bpy.ops.object.delete();    
+
+#Generate facets
+for i in range(-rep1,rep1):
+    for j in range(-rep2,rep2):
+        addFacet(i,j,0);
+#Merge facets together
+for ob in scene.objects:
+    if ob.name[0:5]=='facet' and ob.type=='MESH':
+        ob.select=True;
+bpy.context.scene.objects.active = bpy.data.objects["facet"];
+bpy.ops.object.join();
+#Remove excess points
+bpy.context.scene.objects.active = bpy.data.objects["facet"];
+bpy.ops.object.mode_set(mode='EDIT');
+bpy.ops.mesh.remove_doubles();
+bpy.ops.object.mode_set(mode='OBJECT');
+#Create Box
+addCube(lenX/2,lenY/2,lenZ/2,"bottom");
+selectActive("bottom");
+chopper = bpy.data.objects["bottom"].modifiers.new('Booh', 'BOOLEAN');
+chopper.object = bpy.data.objects["facet"];
+chopper.operation = 'INTERSECT';
+bpy.ops.object.modifier_apply(apply_as='DATA',modifier="Booh");
+delete("facet");
+
+addCube(lenX/2,lenY/2,lenZ/2,"top");
+selectActive("top");
+chopper = bpy.data.objects["top"].modifiers.new('Booh', 'BOOLEAN');
+chopper.object = bpy.data.objects["bottom"];
+chopper.operation = 'DIFFERENCE';
+bpy.ops.object.modifier_apply(apply_as='DATA',modifier="Booh");
+
+#Convert as many triangles to parallelograms as possible
+selectActive("bottom");
+bpy.ops.object.mode_set(mode='EDIT');
+bpy.ops.mesh.quads_convert_to_tris(); #bpy.ops.mesh.tris_convert_to_quads(limit=180);
+bpy.ops.object.mode_set(mode='OBJECT');
+selectActive("top");
+bpy.ops.object.mode_set(mode='EDIT');
+bpy.ops.mesh.quads_convert_to_tris(); #bpy.ops.mesh.tris_convert_to_quads(limit=180);
+bpy.ops.object.mode_set(mode='OBJECT');
+
+#Render settings
+bpy.context.scene.render.use_freestyle = True;
+bpy.context.scene.render.line_thickness = 2.;
+bpy.context.scene.render.resolution_y = bpy.context.scene.render.resolution_x;
+bpy.context.scene.render.alpha_mode = 'TRANSPARENT';
+
+#World and lighting settings
+bpy.context.scene.world.light_settings.use_environment_light = True;
+bpy.context.scene.world.light_settings.environment_energy = .3;
+bpy.context.scene.world.light_settings.use_ambient_occlusion = True;
+
+
+allSelect(False);
+select("top");
+bpy.ops.transform.translate(value=(translation[0],translation[1],translation[2] + seperation/2.));
+select("bottom");
+bpy.ops.transform.translate(value=(translation[0],translation[1],translation[2] - seperation/2.));
---
+++ scripts/blender/crystal1.py
@@ -0,0 +1,37 @@
+import bpy
+
+scale = 5.; # Coordinate scaling
+max = 0.5; # Maximum value for color shading
+
+
+crystal1Tex = bpy.data.textures.new("crystal1Tex",type="VOXEL_DATA");
+crystal1Tex.voxel_data.file_format = "BLENDER_VOXEL";
+crystal1Tex.voxel_data.filepath = "/home/brandon/LANL_2013/Wield/voxels.bvox"
+crystal1Tex.voxel_data.extension = "REPEAT"
+crystal1Tex.use_color_ramp = True;
+crystal1Tex.color_ramp.elements.new(0.00*max).color=(0,0,1,0.2);
+crystal1Tex.color_ramp.elements.remove(crystal1Tex.color_ramp.elements[0]);
+crystal1Tex.color_ramp.elements.remove(crystal1Tex.color_ramp.elements[1]);
+crystal1Tex.color_ramp.elements.new(0.25*max).color=(0,1,1,1);
+crystal1Tex.color_ramp.elements.new(0.50*max).color=(0,1,0,1);
+crystal1Tex.color_ramp.elements.new(0.75*max).color=(1,1,0,1);
+crystal1Tex.color_ramp.elements.new(1.00*max).color=(1,0,0,1);
+#crystal1Tex.use_map_density;
+#crystal1Tex.use_map_emission;
+
+crystal1Mat = bpy.data.materials.new("crystal1Mat");
+crystal1Mat.type = "VOLUME"
+crystal1Mat.volume.density=0.;
+crystal1Mat.volume.density_scale=10.;
+crystal1Mat.volume.step_size = 0.01;
+crystal1TextureSlot = crystal1Mat.texture_slots.add();
+crystal1TextureSlot.texture = crystal1Tex;
+crystal1TextureSlot.texture_coords = "GLOBAL"
+crystal1TextureSlot.use_map_density = True;
+crystal1TextureSlot.density_factor = 4.;
+crystal1TextureSlot.use_map_emission = True;
+crystal1TextureSlot.emission_factor = 2.;
+crystal1TextureSlot.scale = [scale,scale,scale];
+
+
+
