d70b086f1733b30c3d836a89b0eb6aa2396a91f5
Brandon Runnels <brunnels@uccs.edu>
2016-09-30 10:28:15 -0600

    added some much needed documentation

--- src/Main/wieldEnergy1D.h
+++ src/Main/wieldEnergy1D.h
@@ -1,3 +1,128 @@
+///
+/// Input parameters
+/// ================
+/// 
+/// This section describes the possible input parameters used by Wield to generate a 1D energy curve parameterized by a free variable \f$\theta\f$.
+/// Note that the description of some parameters are condensed: for instance $Alpha(X/Y/Z)(1/2) means that there are six variables, $AlphaX1, $AlphaX2, $AlphaY1, and so on.
+/// Paramaters can be specified in the input file, or can be passed through the command line using the -D flag.
+///
+/// Crystal structure
+/// -----------------
+///
+/// \param $Alpha(X/Y/Z)(1/2) Dimension of the unit cell X/Y/Z direction for lattice 1/2
+/// \param $Order(1/2) Number of terms in the Fourier series to keep
+/// \param $(X/Y/Z)(1/2) A list of all X/Y/Z coordinates of each atom in the unit cell of lattice 1/2. 
+///        Boundary atoms will be included, atoms outside the unit cell will be ignored.
+///        \b Note: dimensions must match for each lattice, i.e. $X1, $Y1, $Z1 must have the same number of items in the list.
+/// 
+/// Example for constructing an FCC crystal for lattice 1:
+///
+///       $a 3.615
+///       $Order1 8
+///       $AlphaX1 $a
+///       $AlphaY1 $a
+///       $AlphaZ1 $a
+///       $X1 0.    0.     0.     0.     0. $a/2. -$a/2.  $a/2. -$a/2. $a/2.  $a/2. -$a/2. -$a/2.
+///       $Y1 0. $a/2.  $a/2. -$a/2. -$a/2.    0.     0.     0.     0. $a/2. -$a/2.  $a/2. -$a/2.
+///       $Z1 0. $a/2. -$a/2.  $a/2. -$a/2. $a/2.  $a/2. -$a/2. -$a/2.    0.     0.     0.     0.
+///
+/// Note the use of the variable $a in constructing the crystal structure.
+/// Also notice that the choice of unit cell is shifted from the usual FCC unit cell so that there is an atom at the origin.
+///
+/// Numerical parameters
+/// --------------------
+///
+/// \param $Sigma1/2 Nondimensionalized temperature for lattice 1,2
+/// \param $Epsilon Numerical window function parameter
+/// 
+/// Example:
+///
+///       $Epsilon 0.5
+///       $Sigma1 0.05
+///
+/// Ground state energy
+/// -------------------
+///
+/// The ground state energy is the minimum possible energy that can be taken by the boundary. 
+/// It is a particular orientation of the interface when the top and bottom crystals are identical.
+/// 
+/// \param $Ground Provide a numerical value for the ground state energy (depricated -- use the other variables instead.)
+/// \param $Ground(X/Z)(1/2) Compute the ground state by providing the X and Z vectors corresponding to the minimum energy state. 
+///
+/// The Ground variables are always the same for all FCC bicrystals and for all BCC bicrystals
+/// Generally, the Z axis should correspond to one of the slip planes in the crystal, and then the choice of X axis needs only to be orthogonal.
+/// Example:
+///
+///       #For FCC:
+///       $GroundZ1 1 1 1
+///       $GroundZ2 1 1 1
+///       $GroundX1 -1 1 0
+///       $GroundX2 -1 1 0
+///       #For BCC:
+///       $GroundZ1 1 1 0
+///       $GroundZ2 1 1 0
+///       $GroundX1 0 0 1
+///       $GroundX2 0 0 1
+///
+/// Orientation parameters
+/// ----------------------
+/// 
+/// All rotations are given in degrees.
+///
+/// \param $phi1_1/2 The first Bunge Euler angle for lattice 1/2 (default 0)
+/// \param $Phi_1/2 The second Bunge Euler angle for lattice 1/2 (default 0)
+/// \param $phi2_1/2 The third Bunge Euler angle for lattice 1/2 (default 0)
+/// \param $Axis(X/Y/Z)(1/2) Generate a rotation matrix based on miller indices.  (default: none)
+///        E.g. to generate a rotation such that the for lattice 1 X axis corresponds to the [111] direction and the Z axis corresponds to [10-1], set $AxisX1 to 1 1 1 and $AxisZ1 to 1 0 -1.
+///        \b Note: for each lattice, specify only two of the three possible axes -- the third will be automatically computed.
+///        \b Note: the right hand rule must be obeyed, but the vectors do not have to be orthonormal.
+/// \param $RotAxes1/2 An arbitrarily long list of axes about which to rotate the lattice. \b Note: using $RotAxes1/2 and $Rots1/2 is the preferred way to configure lattices.
+/// \param $Rots1/2 The angles by which to rotate each of the above axes. \b Note: number of rotation angles must correspond to the number of axes in $RotAxes1/2.
+///
+/// 1D range of rotations
+/// ---------------------
+/// 
+/// \f$\theta\f$ is a free variable that is used to evaluate energy for a continuous range of rotations.
+/// It is used to generate rotations for each lattice that are in addition to the orientation rotations (above).
+/// \f$\theta\f$ ranges from ThetaMin to ThetaMax and is incremented by dTheta.
+/// The ThetaRot parameters determine how much rotation to apply to each; for instance in symmetric tilt about the X axis, the following would be set:
+///
+///      $ThetaRotX1 0.5
+///      $ThetaRotX1 -0.5
+///
+/// The rotation composition and order of operations is given by the following:
+/// \f[ R_{total}(\theta) = R_{X}(($ThetaRotX1) \, \theta) R_{Y}(($ThetaRotY1) \, \theta) R_{Z}(($ThetaRotZ1) \, \theta) R_{orientation} \f]
+/// By default, all Theta variables are zero so that if none of them are set, only the energy for the original orientation will be computed.
+///
+/// \param $ThetaMin Minimum value of \f$\theta\f$ (default: 0)
+/// \param $ThetaMax Maximum value of \f$\theta\f$ (default: 0)
+/// \param $dTheta Value by which to increment \f$\theta\f$ (default: 0)
+/// \param $ThetaRot(X/Y/Z)(1/2) Amount of rotation of lattice 1/2 about axis X/Y/Z
+///   
+/// Example usage for symmetric tilt about the X axis from 0 to 90 degrees:
+///
+///       $ThetaMin 0
+///       $ThetaMax 90
+///       $DTheta 1
+///       $ThetaRotX1 0.5
+///       $ThetaRotX1 -0.5
+///
+/// Output file
+/// -----------
+///
+/// All information is output to a single output file specified by OutFile:
+///
+/// \param $OutFile The path to the output file
+///
+
+
+
+///
+/// \file wieldEnergy1D.h
+/// \brief This is the short description for wieldEnergy1D.h
+///
+
+
 #ifndef WIELD_MAIN_ENERGY1D_H
 #define WIELD_MAIN_ENERGY1D_H
 
