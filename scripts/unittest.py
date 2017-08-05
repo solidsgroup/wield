84a9e6fef3090202968de8f457d9079dd9e526f8
Brandon Runnels <brunnels@uccs.edu>
2017-08-05 11:09:54 -0600

    updated unit tests

--- scripts/unittest.py
+++ scripts/unittest.py
@@ -2,15 +2,23 @@ import sys
 import subprocess
 import filecmp
 
-print(sys.argv[1]+"...........",end='')
-with open(sys.argv[1]+"/run.stdout","w") as stdoutfile:
-    if subprocess.call(["./bin/wield",sys.argv[1]+"/input.in","-DOutFile="+sys.argv[1]+"/output.dat"],stdout=stdoutfile):
-        print('\033[31m\033[1mFAIL\033[0m')
+print(sys.argv[1] + "."*(60-len(sys.argv[1])),end='')
+sys.stdout.flush()
+try:
+    with open(sys.argv[1]+"run.stdout","w") as stdoutfile:
+        err=subprocess.call(["./bin/wield",sys.argv[1]+"input.in","-DOutFile="+sys.argv[1]+"output.dat"],stdout=stdoutfile);
+        if err:
+            print('\033[31m\033[1mFAIL\033[0m')
+            raise Exception('Executable failed with error code '.str(err))
+
+    if not filecmp.cmp(sys.argv[1]+"output.dat",sys.argv[1]+"output.ref"):
+        raise Exception("File comparison failed")
         sys.exit(0)
 
-if not filecmp.cmp(sys.argv[1]+"/output.dat",sys.argv[1]+"/output.ref"):
-    print('\033[31m\033[1mFAIL\033[0m')
+    print('\033[32m\033[1mPASS\033[0m')
     sys.exit(0)
-
-print('\033[32m\033[1mPASS\033[0m')
-sys.exit(0)
+except Exception  as e:
+    print('\033[31m\033[1mFAIL\033[0m')
+    with open(sys.argv[1]+"run.error","a") as stderrfile:
+        stderrfile.write(str(e))
+    sys.exit(0)
