91d1c6ee7e7ceffa1e219ec8f4283a8172c19aff
Brandon Runnels <brunnels@uccs.edu>
2019-11-08 16:03:11 -0700

    Added the file specifier for rotations. Also updated some formatting

--- src/Main/wieldCSL.h
+++ src/Main/wieldCSL.h
@@ -26,12 +26,12 @@ namespace Main
 
 /// Coincident Site Lattice Computation
 /// ===================================
-/// 
+///
 /// Compute the Coincident Site Lattice \f$\Sigma\f$ value for a bicrystal.
 /// The setup for this function is very similar to that for grain boundary computations.
 /// Here is an example input file to compute \f$\Sigma\f$ for FCC aluminum tilte about the
 /// [100] axis:
-/// 
+///
 ///     $CSL
 ///     $a 3.597
 ///     $Sigma1 0.06
@@ -52,167 +52,230 @@ namespace Main
 ///     $OutFile csl_$Sigma1_$Epsilon.dat
 ///
 /// Additional examples are included in the tests directory.
-/// 
+///
 /// **Relevant Publications**
 /// - B Runnels,
 ///   [A projection-based formulation of the coincident site lattice \f$\Sigma\f$ for arbitrary bicrystals at finite temperature](http://scripts.iucr.org/cgi-bin/paper?S205327331700122X)
 ///   *Acta Crystallographica A*, 2017
 void CSL(Reader::Reader &reader, ///< [in] Reader object to parse input commands
-	 int numThreads=1,       ///< [in] Number of OpenMP threads to spawn
-	 bool verbose=false      ///< [in] Whether to include extra output information
-	 )
+		 int numThreads = 1,	 ///< [in] Number of OpenMP threads to spawn
+		 bool verbose = false	///< [in] Whether to include extra output information
+)
 {
-  WIELD_EXCEPTION_TRY;
-  if(verbose) WIELD_NOTE("Beginning CSL Computation")
-  if(verbose) WIELD_NOTE("Reading in parameters")
-
-  double 
-     thetaMin  = reader.Read<double>("ThetaMin",0.),
-     thetaMax  = reader.Read<double>("ThetaMax",0.),
-     dTheta    = reader.Read<double>("DTheta",1.),
-    // rMin      = reader.Read<double>("RMin",0.),
-    // rMax      = reader.Read<double>("RMax",1.),
-    // dR        = reader.Read<double>("DR",.05),
-    epsilon   = reader.Read<double>("Epsilon",1.),
-    tolerance = reader.Read<double>("Tolerance",1E-16);
-
-  double
-    thetaRotX1 = reader.Read<double>("ThetaRotX1",0.),
-    thetaRotY1 = reader.Read<double>("ThetaRotY1",0.),
-    thetaRotZ1 = reader.Read<double>("ThetaRotZ1",0.),
-    thetaRotX2 = reader.Read<double>("ThetaRotX2",0.),
-    thetaRotY2 = reader.Read<double>("ThetaRotY2",0.),
-    thetaRotZ2 = reader.Read<double>("ThetaRotZ2",0.);
-
-  Wield::Series::FourierSeries<Wield::Series::SqrtGaussDirac> *C1, *C2;
-
-  if(verbose) WIELD_NOTE("Generating crystal 1");
-
-  
-  C1 = new Wield::Series::FourierSeries<Wield::Series::SqrtGaussDirac>
-    (reader.Read<int>("Order1"),
-     reader.Read<double>("AlphaX1"),
-     reader.Read<double>("AlphaY1"),
-     reader.Read<double>("AlphaZ1"),
-     Wield::Series::SqrtGaussDirac(reader.Read<double>("Sigma1")),
-     reader.Read<std::vector<double> >("X1"),
-     reader.Read<std::vector<double> >("Y1"),
-     reader.Read<std::vector<double> >("Z1"),
-     verbose);
-  
-  if (reader.Find("Order2") 
-      && reader.Find("AlphaX2") && reader.Find("AlphaY2") && reader.Find("AlphaZ2") 
-      && reader.Find("Sigma2") 
-      && reader.Find("X2") && reader.Find("Y2") && reader.Find("Z2"))
-    {
-      if(verbose) WIELD_NOTE("Generating crystal 2");
-      C2 = new Wield::Series::FourierSeries<Wield::Series::SqrtGaussDirac>(reader.Read<int>("Order2"),
-									     reader.Read<double>("AlphaX2"),
-									     reader.Read<double>("AlphaY2"),
-									     reader.Read<double>("AlphaZ2"),
-									     Wield::Series::SqrtGaussDirac(reader.Read<double>("Sigma2")),
-									     reader.Read<std::vector<double> >("X2"),
-									     reader.Read<std::vector<double> >("Y2"),
-									     reader.Read<std::vector<double> >("Z2"),
-									     verbose);
-    }
-  else
-    {
-      if(verbose) WIELD_NOTE("Creating a copy of crystal 1");
-      C2 = new Wield::Series::FourierSeries<Wield::Series::SqrtGaussDirac>(*C1);
-    }
-
-  //
-  // Rotation matrix 1
-  //
-  if(verbose) WIELD_NOTE("Computing rotation matrices")
-
-  Eigen::Matrix3d rot1 = Eigen::Matrix3d::Identity();
-  if (reader.Find("AxisY1") && reader.Find("AxisZ1"))
-    rot1 = createMatrixFromYZ(reader.Read<Eigen::Vector3d>("AxisY1"),reader.Read<Eigen::Vector3d>("AxisZ1")).transpose() * rot1;
-  else if (reader.Find("AxisZ1") && reader.Find("AxisX1"))
-    rot1 = createMatrixFromZX(reader.Read<Eigen::Vector3d>("AxisZ1"),reader.Read<Eigen::Vector3d>("AxisX1")).transpose() * rot1; // added transpose
-  else if (reader.Find("AxisX1") && reader.Find("AxisY1"))
-    rot1 = createMatrixFromXY(reader.Read<Eigen::Vector3d>("AxisX1"),reader.Read<Eigen::Vector3d>("AxisY1")).transpose() * rot1; // added transpose
-  else if (reader.Find("RotAxes1"))
-    {
-      std::vector<char> rotAxes1 = reader.Read<std::vector<char> >("RotAxes1");
-      std::vector<double>  rots1 = reader.Read<std::vector<double> >("Rots1");
-      for (int i=0; i<rotAxes1.size(); i++) rot1 = createMatrixFromAngle(rots1[i],rotAxes1[i]) * rot1;
-    }
-  if (verbose) WIELD_NOTE("Rot1 components: " << std::endl << rot1);
-  if (verbose) WIELD_NOTE("Rot1 Euler angles: " << rot1.eulerAngles(2,0,2).transpose()*180./pi);
-
-  //
-  // Rotation matrix 2
-  //
-
-  Eigen::Matrix3d rot2 = Eigen::Matrix3d::Identity();
-  if (reader.Find("AxisY2") && reader.Find("AxisZ2")) 
-    rot2 = createMatrixFromYZ(reader.Read<Eigen::Vector3d>("AxisY2"),reader.Read<Eigen::Vector3d>("AxisZ2")).transpose() * rot2; // added transpose
-  else if (reader.Find("AxisZ2") && reader.Find("AxisX2")) 
-    rot2 = createMatrixFromZX(reader.Read<Eigen::Vector3d>("AxisZ2"),reader.Read<Eigen::Vector3d>("AxisX2")).transpose() * rot2; // added transpose
-  else if (reader.Find("AxisX2") && reader.Find("AxisY2")) 
-    rot2 = createMatrixFromXY(reader.Read<Eigen::Vector3d>("AxisX2"),reader.Read<Eigen::Vector3d>("AxisY2")).transpose() * rot2; // added transpose
-  else if (reader.Find("RotAxes2"))
-    {
-      std::vector<char> rotAxes2 = reader.Read<std::vector<char> >("RotAxes2");
-      std::vector<double>  rots2 = reader.Read<std::vector<double> >("Rots2");
-      for (int i=0; i<rotAxes2.size(); i++) rot2 = createMatrixFromAngle(rots2[i],rotAxes2[i]) * rot2;
-    }
-  if (verbose) WIELD_NOTE("Rot2 components: " << std::endl << rot2);
-  if (verbose) WIELD_NOTE("Rot2 Euler angles: " << rot2.eulerAngles(2,0,2).transpose()*180./pi);
+	WIELD_EXCEPTION_TRY;
+	if (verbose)
+		WIELD_NOTE("Beginning CSL Computation")
+	if (verbose)
+		WIELD_NOTE("Reading in parameters")
 
 
-  std::vector<double> thetas;
-  for (double theta = thetaMin; theta <= thetaMax ; theta += dTheta) thetas.push_back(theta);
-  std::vector<double> sigmas(thetas.size());
-  if(verbose) WIELD_NOTE("Beginning computation: with thetaMin="<<thetaMin<<", thetaMax="<<thetaMax<<", dTheta="<<dTheta<<", # of computations = "<<thetas.size());
+	double
+		thetaMin = reader.Read<double>("ThetaMin", 0.),
+		thetaMax = reader.Read<double>("ThetaMax", 0.),
+		dTheta = reader.Read<double>("DTheta", 1.),
+		// rMin      = reader.Read<double>("RMin",0.),
+		// rMax      = reader.Read<double>("RMax",1.),
+		// dR        = reader.Read<double>("DR",.05),
+		epsilon = reader.Read<double>("Epsilon", 1.),
+		tolerance = reader.Read<double>("Tolerance", 1E-16);
 
-  
-  if (verbose) WIELD_NOTE("Computing norm of crystal 1");
-  double c1c1 = Wield::Integrator::Volume(*C1,rot1,*C1,rot1,epsilon,tolerance,false);
-  if (verbose) WIELD_NOTE("Computing norm of crystal 2");
-  double c2c2 = Wield::Integrator::Volume(*C2,rot2,*C2,rot2,epsilon,tolerance,false); 
-  
-  #pragma omp parallel for num_threads(numThreads)
-  for ( int i = 0 ; i < thetas.size(); i++)
-    {
-      int index = omp_get_thread_num();
-      //if (index != 0) verbose = false;
+	double
+		thetaRotX1 = reader.Read<double>("ThetaRotX1", 0.),
+		thetaRotY1 = reader.Read<double>("ThetaRotY1", 0.),
+		thetaRotZ1 = reader.Read<double>("ThetaRotZ1", 0.),
+		thetaRotX2 = reader.Read<double>("ThetaRotX2", 0.),
+		thetaRotY2 = reader.Read<double>("ThetaRotY2", 0.),
+		thetaRotZ2 = reader.Read<double>("ThetaRotZ2", 0.);
 
-      //if (verbose){ WIELD_NOTE("Computing for theta " << i << " / " << thetas.size());}
-      if (index==0) WIELD_PROGRESS("Computing CSL", thetas[i]-thetaMin, (thetaMax-thetaMin)/numThreads, dTheta);
+	Wield::Series::FourierSeries<Wield::Series::SqrtGaussDirac> *C1, *C2;
 
-      Eigen::Matrix3d
-	omega1 =
-	createMatrixFromXAngle(thetaRotX1*thetas[i]) *
-	createMatrixFromYAngle(thetaRotY1*thetas[i]) *
-	createMatrixFromZAngle(thetaRotZ1*thetas[i]) *
-	rot1,
-	omega2 =
-	createMatrixFromXAngle(thetaRotX2*thetas[i]) *
-	createMatrixFromYAngle(thetaRotY2*thetas[i]) *
-	createMatrixFromZAngle(thetaRotZ2*thetas[i]) *
-	rot2;      
-      double 
-	c1c2 = Wield::Integrator::Volume(*C1,omega1,*C2,omega2,epsilon,tolerance,false);
+	if (verbose)
+		WIELD_NOTE("Generating crystal 1");
 
-      sigmas[i] = sqrt(c1c1)*sqrt(c2c2) / c1c2;
-    }
-  WIELD_PROGRESS_COMPLETE("Computing CSL");
+	C1 = new Wield::Series::FourierSeries<Wield::Series::SqrtGaussDirac>(
+				reader.Read<int>("Order1"),
+				reader.Read<double>("AlphaX1"),
+				reader.Read<double>("AlphaY1"),
+				reader.Read<double>("AlphaZ1"),
+				Wield::Series::SqrtGaussDirac(reader.Read<double>("Sigma1")),
+				reader.Read<std::vector<double>>("X1"),
+				reader.Read<std::vector<double>>("Y1"),
+				reader.Read<std::vector<double>>("Z1"),
+				numThreads, verbose);
 
-  std::ofstream out(reader.Read<std::string>("OutFile").c_str());
+	if (reader.Find("Order2") && 
+	reader.Find("AlphaX2") && reader.Find("AlphaY2") && reader.Find("AlphaZ2") 
+	&& reader.Find("Sigma2") && reader.Find("X2") && reader.Find("Y2") && reader.Find("Z2"))
+	{
+		if (verbose)
+			WIELD_NOTE("Generating crystal 2");
+		C2 = new Wield::Series::FourierSeries<Wield::Series::SqrtGaussDirac>(
+				reader.Read<int>("Order2"),
+				reader.Read<double>("AlphaX2"),
+				reader.Read<double>("AlphaY2"),
+				reader.Read<double>("AlphaZ2"),
+				Wield::Series::SqrtGaussDirac(reader.Read<double>("Sigma2")),
+				reader.Read<std::vector<double>>("X2"),
+				reader.Read<std::vector<double>>("Y2"),
+				reader.Read<std::vector<double>>("Z2"),
+				numThreads, verbose);
+	}
+	else
+	{
+		if (verbose)
+			WIELD_NOTE("Creating a copy of crystal 1");
+		C2 = new Wield::Series::FourierSeries<Wield::Series::SqrtGaussDirac>(*C1);
+	}
 
-  for (int i=0; i<thetas.size(); i++)
-    out << thetas[i] << " " << sigmas[i] << std::endl;
-  out.close();
+	//
+	// Rotation matrix 1
+	//
+	if (verbose)
+		WIELD_NOTE("Computing rotation matrices")
 
+	Eigen::Matrix3d rot1 = Eigen::Matrix3d::Identity();
+	if (reader.Find("AxisY1") && reader.Find("AxisZ1"))
+		rot1 = createMatrixFromYZ(reader.Read<Eigen::Vector3d>("AxisY1"), reader.Read<Eigen::Vector3d>("AxisZ1")).transpose() * rot1;
+	else if (reader.Find("AxisZ1") && reader.Find("AxisX1"))
+		rot1 = createMatrixFromZX(reader.Read<Eigen::Vector3d>("AxisZ1"), reader.Read<Eigen::Vector3d>("AxisX1")).transpose() * rot1; // added transpose
+	else if (reader.Find("AxisX1") && reader.Find("AxisY1"))
+		rot1 = createMatrixFromXY(reader.Read<Eigen::Vector3d>("AxisX1"), reader.Read<Eigen::Vector3d>("AxisY1")).transpose() * rot1; // added transpose
+	else if (reader.Find("RotAxes1"))
+	{
+		std::vector<char> rotAxes1 = reader.Read<std::vector<char>>("RotAxes1");
+		std::vector<double> rots1 = reader.Read<std::vector<double>>("Rots1");
+		for (int i = 0; i < rotAxes1.size(); i++)
+			rot1 = createMatrixFromAngle(rots1[i], rotAxes1[i]) * rot1;
+	}
+	if (verbose)
+		WIELD_NOTE("Rot1 components: " << std::endl
+									   << rot1);
+	if (verbose)
+		WIELD_NOTE("Rot1 Euler angles: " << rot1.eulerAngles(2, 0, 2).transpose() * 180. / pi);
 
+	//
+	// Rotation matrix 2
+	//
 
-  WIELD_EXCEPTION_CATCH;
+	Eigen::Matrix3d rot2 = Eigen::Matrix3d::Identity();
+	if (reader.Find("AxisY2") && reader.Find("AxisZ2"))
+		rot2 = createMatrixFromYZ(reader.Read<Eigen::Vector3d>("AxisY2"), reader.Read<Eigen::Vector3d>("AxisZ2")).transpose() * rot2; // added transpose
+	else if (reader.Find("AxisZ2") && reader.Find("AxisX2"))
+		rot2 = createMatrixFromZX(reader.Read<Eigen::Vector3d>("AxisZ2"), reader.Read<Eigen::Vector3d>("AxisX2")).transpose() * rot2; // added transpose
+	else if (reader.Find("AxisX2") && reader.Find("AxisY2"))
+		rot2 = createMatrixFromXY(reader.Read<Eigen::Vector3d>("AxisX2"), reader.Read<Eigen::Vector3d>("AxisY2")).transpose() * rot2; // added transpose
+	else if (reader.Find("RotAxes2"))
+	{
+		std::vector<char> rotAxes2 = reader.Read<std::vector<char>>("RotAxes2");
+		std::vector<double> rots2 = reader.Read<std::vector<double>>("Rots2");
+		for (int i = 0; i < rotAxes2.size(); i++)
+			rot2 = createMatrixFromAngle(rots2[i], rotAxes2[i]) * rot2;
+	}
+	if (verbose)
+		WIELD_NOTE("Rot2 components: " << std::endl
+									   << rot2);
+	if (verbose)
+		WIELD_NOTE("Rot2 Euler angles: " << rot2.eulerAngles(2, 0, 2).transpose() * 180. / pi);
+
+	std::vector<Eigen::Matrix3d> omega1s, omega2s;
+	std::vector<double> thetas;
+	if (reader.Find("OrientationFile"))
+	{
+		std::string filename = reader.Read<std::string>("OrientationFile");
+		std::string line;
+		if (verbose) WIELD_NOTE("Reading from file " + filename);
+		std::ifstream inputfile(filename);
+		if (inputfile.is_open())
+		{
+			getline(inputfile,line); // ignore first line
+			int ctr = 0;
+			while (getline(inputfile,line))
+			{
+				//std::cout << line << std::endl;
+				std::stringstream tokenizer(line);
+				std::string tmp;
+				std::vector<double> dat;
+				while (getline(tokenizer,tmp,' '))
+				{
+					dat.push_back(atof(tmp.c_str()));
+				}
+				Eigen::Matrix3d rot1, rot2;
+				rot1(0,0) = dat[ 0]; rot1(0,1) = dat[ 3]; rot1(0,2) = dat[ 6];
+				rot1(1,0) = dat[ 1]; rot1(1,1) = dat[ 4]; rot1(1,2) = dat[ 7];
+				rot1(2,0) = dat[ 2]; rot1(2,1) = dat[ 5]; rot1(2,2) = dat[ 8];
+				
+				rot2(0,0) = dat[ 9]; rot2(0,1) = dat[12]; rot2(0,2) = dat[15];
+				rot2(1,0) = dat[10]; rot2(1,1) = dat[13]; rot2(1,2) = dat[16];
+				rot2(2,0) = dat[11]; rot2(2,1) = dat[14]; rot2(2,2) = dat[17];
+
+				thetas.push_back(ctr);
+				omega1s.push_back(rot1.transpose());
+				omega2s.push_back(rot2.transpose());
+				ctr++;
+
+				//std::cout << "R1^TR1 = \n" << rot1.transpose()*rot1 << std::endl;
+				//std::cout << "R2^TR2 = \n" << rot2.transpose()*rot2 << std::endl;
+				//std::cout <<  rot1*rot2.transpose() << std::endl << std::endl;
+			}
+			
+		}
+	}
+	else
+	{
+		for (double theta = thetaMin; theta <= thetaMax; theta += dTheta)
+		{
+			omega1s.push_back(createMatrixFromXAngle(thetaRotX1 * theta) *
+							  createMatrixFromYAngle(thetaRotY1 * theta) *
+							  createMatrixFromZAngle(thetaRotZ1 * theta) *
+							  rot1);
+			omega2s.push_back(createMatrixFromXAngle(thetaRotX2 * theta) *
+							  createMatrixFromYAngle(thetaRotY2 * theta) *
+							  createMatrixFromZAngle(thetaRotZ2 * theta) *
+							  rot2);
+			thetas.push_back(theta);
+		}
+	}
+	std::vector<double> sigmas(thetas.size());
+	if (verbose)
+		WIELD_NOTE("Beginning computation: with thetaMin=" << thetaMin << ", thetaMax=" << thetaMax << ", dTheta=" << dTheta << ", # of computations = " << thetas.size());
+
+	if (verbose)
+		WIELD_NOTE("Computing norm of crystal 1");
+	double c1c1 = Wield::Integrator::Volume(*C1, rot1, *C1, rot1, epsilon, tolerance, false);
+	if (verbose)
+		WIELD_NOTE("Computing norm of crystal 2");
+	double c2c2 = Wield::Integrator::Volume(*C2, rot2, *C2, rot2, epsilon, tolerance, false);
+
+#pragma omp parallel for num_threads(numThreads)
+	for (int i = 0; i < thetas.size(); i++)
+	{
+		int index = omp_get_thread_num();
+		//if (index != 0) verbose = false;
+
+		//if (verbose){ WIELD_NOTE("Computing for theta " << i << " / " << thetas.size());}
+		if (index == 0)
+			WIELD_PROGRESS("Computing CSL", (float)i, (float)(thetas.size() - i) / numThreads, dTheta);
+
+		Eigen::Matrix3d
+		omega1 = omega1s[i],
+		omega2 = omega2s[i];
+
+		double
+			c1c2 = Wield::Integrator::Volume(*C1, omega1, *C2, omega2, epsilon, tolerance, false);
+
+		sigmas[i] = sqrt(c1c1) * sqrt(c2c2) / c1c2;
+	}
+	WIELD_PROGRESS_COMPLETE("Computing CSL");
+
+	std::ofstream out(reader.Read<std::string>("OutFile").c_str());
+
+	for (int i = 0; i < thetas.size(); i++)
+		out << thetas[i] << " " << sigmas[i] << std::endl;
+	out.close();
+
+	WIELD_EXCEPTION_CATCH;
 }
-}
-}
+} // namespace Main
+} // namespace Wield
 
 #endif
--- src/Series/wieldFourierSeries.h
+++ src/Series/wieldFourierSeries.h
@@ -20,115 +20,118 @@ namespace Wield
 namespace Series
 {
 
-template<class Mollifier>
+template <class Mollifier>
 class FourierSeries
 {
-public:  
-  
-  // convolution function: exp(||x||^2 / sigma^2) / (sigma sqrt(pi))^3
-  // FS function: exp(- sigma^2||k||^2 / 4) / (2 pi)^{3/2}
-  // inline double WHat(double x, double y, double z)
-  // {
-  //   return exp(-sigma*sigma*(x*x + y*y + z*z)/4.) / sqrt(8*pi*pi*pi);
-  // }
+public:
+	// convolution function: exp(||x||^2 / sigma^2) / (sigma sqrt(pi))^3
+	// FS function: exp(- sigma^2||k||^2 / 4) / (2 pi)^{3/2}
+	// inline double WHat(double x, double y, double z)
+	// {
+	//   return exp(-sigma*sigma*(x*x + y*y + z*z)/4.) / sqrt(8*pi*pi*pi);
+	// }
 
-  FourierSeries(const FourierSeries<Mollifier> &copy):
-    order(copy.order),
-    alphaX(copy.alphaX),
-    alphaY(copy.alphaY),
-    alphaZ(copy.alphaZ),
-    phiHat(copy.phiHat),
-    sigma(copy.sigma),
-    C(copy.C) {}
+	FourierSeries(const FourierSeries<Mollifier> &copy) : order(copy.order),
+														  alphaX(copy.alphaX),
+														  alphaY(copy.alphaY),
+														  alphaZ(copy.alphaZ),
+														  phiHat(copy.phiHat),
+														  sigma(copy.sigma),
+														  C(copy.C) {}
 
-  FourierSeries(int _order, 
-		double _alphaX, 
-		double _alphaY, 
-		double _alphaZ,
-		//double _sigma, 
-		Mollifier _mollifier,
-		std::vector<double> X, 
-		std::vector<double> Y, 
-		std::vector<double> Z,
-		int verbose=false):
-    order(_order),
-    alphaX(_alphaX),
-    alphaY(_alphaY),
-    alphaZ(_alphaZ),
-    //sigma(_sigma)
-    phiHat(_mollifier)
-  {
-    C.resize((2*order - 1)*(2*order - 1)*(2*order - 1));
+	FourierSeries(int _order,
+				  double _alphaX,
+				  double _alphaY,
+				  double _alphaZ,
+				  //double _sigma,
+				  Mollifier _mollifier,
+				  std::vector<double> X,
+				  std::vector<double> Y,
+				  std::vector<double> Z,
+				  int numThreads = 1,
+				  int verbose = false) : order(_order),
+										 alphaX(_alphaX),
+										 alphaY(_alphaY),
+										 alphaZ(_alphaZ),
+										 //sigma(_sigma)
+										 phiHat(_mollifier)
+	{
+		C.resize((2 * order - 1) * (2 * order - 1) * (2 * order - 1));
 
-    if ((X.size() != Y.size()) || (Y.size() != Z.size()) || (Z.size() != X.size()))
-      WIELD_EXCEPTION_NEW("Error: X, Y, Z vectors must be the same size");
+		if ((X.size() != Y.size()) || (Y.size() != Z.size()) || (Z.size() != X.size()))
+			WIELD_EXCEPTION_NEW("Error: X, Y, Z vectors must be the same size");
 
-    std::complex<double> I(0,1);
-    int size = X.size();
-    for (int l=-order+1; l<order; l++)
-      {
-	if(verbose) WIELD_PROGRESS("Computing CSL", l+order, 2*order, 1);
-	for (int m=-order+1; m<order; m++)
-	  for (int n=-order+1; n<order; n++)
-	    {
-	      (*this)(l,m,n) = 0;
-	      for (unsigned int i=0; i<size; i++)
+		std::complex<double> I(0, 1);
+		int size = X.size();
+		for (int l = -order + 1; l < order; l++)
 		{
-		  if (fabs(X[i]) > 0.5*alphaX) WIELD_WARNING("X[i] = " << X[i] << " > 0.5*alphaX= " << 0.5*alphaX );
-		  if (fabs(Y[i]) > 0.5*alphaY) WIELD_WARNING("Y[i] = " << Y[i] << " > 0.5*alphaY= " << 0.5*alphaY );
-		  if (fabs(Z[i]) > 0.5*alphaZ) WIELD_WARNING("Z[i] = " << Z[i] << " > 0.5*alphaZ= " << 0.5*alphaZ );
+			if (verbose)
+				WIELD_PROGRESS("Computing CSL", l + order, 2 * order, 1);
+#pragma omp parallel for num_threads(numThreads)
+			for (int m = -order + 1; m < order; m++)
+				for (int n = -order + 1; n < order; n++)
+				{
+					(*this)(l, m, n) = 0;
+					for (unsigned int i = 0; i < size; i++)
+					{
+						if (fabs(X[i]) > 0.5 * alphaX)
+							WIELD_WARNING("X[i] = " << X[i] << " > 0.5*alphaX= " << 0.5 * alphaX);
+						if (fabs(Y[i]) > 0.5 * alphaY)
+							WIELD_WARNING("Y[i] = " << Y[i] << " > 0.5*alphaY= " << 0.5 * alphaY);
+						if (fabs(Z[i]) > 0.5 * alphaZ)
+							WIELD_WARNING("Z[i] = " << Z[i] << " > 0.5*alphaZ= " << 0.5 * alphaZ);
 
-		  double lambda = 1.;
-		  if (fabs(fabs(X[i]) - 0.5*alphaX) < 1E-8) lambda *= 0.5;
-		  if (fabs(fabs(Y[i]) - 0.5*alphaY) < 1E-8) lambda *= 0.5;
-		  if (fabs(fabs(Z[i]) - 0.5*alphaZ) < 1E-8) lambda *= 0.5;
+						double lambda = 1.;
+						if (fabs(fabs(X[i]) - 0.5 * alphaX) < 1E-8)
+							lambda *= 0.5;
+						if (fabs(fabs(Y[i]) - 0.5 * alphaY) < 1E-8)
+							lambda *= 0.5;
+						if (fabs(fabs(Z[i]) - 0.5 * alphaZ) < 1E-8)
+							lambda *= 0.5;
 
-		  Eigen::Covector3d k(2.*pi*(double)l/alphaX, 2.*pi*(double)m/alphaY, 2.*pi*(double)n/alphaZ);
-		  Eigen::Vector3d x(X[i],Y[i],Z[i]);
+						Eigen::Covector3d k(2. * pi * (double)l / alphaX, 2. * pi * (double)m / alphaY, 2. * pi * (double)n / alphaZ);
+						Eigen::Vector3d x(X[i], Y[i], Z[i]);
 
-		  (*this)(l,m,n) +=
-		    lambda
-		    * phiHat(k)
-		    * exp(-I * (double)(k*x))
-		    / (alphaX * alphaY * alphaZ);
+						(*this)(l, m, n) +=
+							lambda * phiHat(k) * exp(-I * (double)(k * x)) / (alphaX * alphaY * alphaZ);
+					}
+				}
 		}
-	    }
-      }
-    if(verbose) WIELD_PROGRESS_COMPLETE("Computing CSL");
-  }
+		if (verbose)
+			WIELD_PROGRESS_COMPLETE("Computing CSL");
+	}
 
-  std::complex<double> & operator() (signed int i, signed int j, signed int k)
-  {
-    return C[(2*order-1)*(2*order-1)*(order + i - 1) + (2*order-1)*(order + j - 1) + (order + k - 1)];
-  }      
+	std::complex<double> &operator()(signed int i, signed int j, signed int k)
+	{
+		return C[(2 * order - 1) * (2 * order - 1) * (order + i - 1) + (2 * order - 1) * (order + j - 1) + (order + k - 1)];
+	}
 
-  std::complex<double> operator() (double X, double Y, double Z)
-  {
-    Eigen::Vector3d x(X,Y,Z);
-    std::complex<double> I(0,1), val(0,0);
-    for (int l=-order+1; l<order; l++)
-      for (int m=-order+1; m<order; m++)
-      	for (int n=-order+1; n<order; n++)
-	  {
-	    Eigen::Covector3d k(2.*pi*(double)l / alphaX, 2.*pi*(double)m / alphaY, 2.*pi*(double)n / alphaZ); 
-	    val += (*this)(l,m,n) * exp(I * (double)(k*x));
-	  }
-    return val;
-  }      
+	std::complex<double> operator()(double X, double Y, double Z)
+	{
+		Eigen::Vector3d x(X, Y, Z);
+		std::complex<double> I(0, 1), val(0, 0);
+		for (int l = -order + 1; l < order; l++)
+			for (int m = -order + 1; m < order; m++)
+				for (int n = -order + 1; n < order; n++)
+				{
+					Eigen::Covector3d k(2. * pi * (double)l / alphaX, 2. * pi * (double)m / alphaY, 2. * pi * (double)n / alphaZ);
+					val += (*this)(l, m, n) * exp(I * (double)(k * x));
+				}
+		return val;
+	}
 
-  // Variables
-  int order;      ///< Number of terms in each direction (times 2 + 1) \details
-  double alphaX;  ///< \brief X Lattice coefficient
-  double alphaY;  ///< \brief Y Lattice coefficient
-  double alphaZ;  ///< \brief Z Lattice coefficient
-  double sigma;
+	// Variables
+	int order;	 ///< Number of terms in each direction (times 2 + 1) \details
+	double alphaX; ///< \brief X Lattice coefficient
+	double alphaY; ///< \brief Y Lattice coefficient
+	double alphaZ; ///< \brief Z Lattice coefficient
+	double sigma;
+
 private:
-  std::vector<std::complex<double> > C;          ///< Matrix of complex Fourier coefficients
-  Mollifier phiHat;
+	std::vector<std::complex<double>> C; ///< Matrix of complex Fourier coefficients
+	Mollifier phiHat;
 };
 
-
-
-}
-}
+} // namespace Series
+} // namespace Wield
 #endif
