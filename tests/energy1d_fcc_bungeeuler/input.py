d073e74b191ace6064f8ce50004c45e4cd9c3990
Brandon Runnels <brunnels@uccs.edu>
2023-01-10 16:55:15 -0500

    fixed by adding GD to Crystal and Surface

--- tests/energy1d_fcc_bungeeuler/input.py
+++ tests/energy1d_fcc_bungeeuler/input.py
@@ -10,7 +10,7 @@ X = [0,   0,    0,    0,    0, a/2, -a/2,  a/2, -a/2, a/2,  a/2, -a/2, -a/2]
 Y = [0, a/2,  a/2, -a/2, -a/2,   0,    0,    0,    0, a/2, -a/2,  a/2, -a/2]
 Z = [0, a/2, -a/2,  a/2, -a/2, a/2,  a/2, -a/2, -a/2,   0,    0,    0,    0]
 
-C1 = wield.CrystalSQGD(order, a, a, a, sigma, X, Y, Z, 1, True)
+C1 = wield.CrystalGD(order, a, a, a, sigma, X, Y, Z, 1, True)
 C2 = C1
 
 #Rground = wield.createMatrixFromBungeEulerAngles(4,0,2)
@@ -26,7 +26,7 @@ theta = 150
 print(theta)
 Rtheta1 = wield.createMatrixFromXAngle(theta)
 Rtheta2 = wield.createMatrixFromXAngle(theta)
-energy  = wield.Surface(C1,Rtheta1 @ R1,C2,Rtheta2 @ R2,eps,tolerance)
+energy  = wield.SurfaceGD(C1,Rtheta1 @ R1,C2,Rtheta2 @ R2,eps,tolerance)
 
 
 with open('output_py.ref', 'w') as f:
