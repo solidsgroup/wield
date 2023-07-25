fe7ec015196d9d9d160d8b1b4fb00f9bb37e96fa
Brandon Runnels <brunnels@iastate.edu>
2023-07-25 15:54:29 -0500

    Update dependencies-apt.sh

--- .github/workflows/dependencies-apt.sh
+++ .github/workflows/dependencies-apt.sh
@@ -1,13 +1,3 @@
 set -eu -o pipefail
-
 sudo apt-get update
-
 sudo apt-get install -y --no-install-recommends build-essential g++ libpython3-dev libeigen3-dev
-
-# Requirements for regression test scripts
-#pip3 install yt matplotlib numpy pandas
-
-# Requirements for building documentation
-#pip3 install sphinx breathe m2r sphinx_rtd_theme linuxdoc sphinx_design sphinx-copybutton
-
-#npm install -g eclint
