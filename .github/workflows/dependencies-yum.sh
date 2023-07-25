65fa0ff7ec7135a879efbee8dd866fba9ff621fa
Brandon Runnels <brunnels@uccs.edu>
2023-07-25 15:41:32 -0500

    Integrate RedHat CI to github actions (#2)
    * container test
    
    * rhel
    
    * increment
    
    * add sudo
    
    * increment
    
    * increment
    
    * incremental
    
    * incrmenetal
    
    * incrmenetal
    
    * incrmenetal
    
    * incrmenetal
    
    * incrmenetal
    
    * incrmenetal
    
    * ok, RH test working now
    
    * trying to streamline docker image

---
+++ .github/workflows/dependencies-yum.sh
@@ -0,0 +1,11 @@
+set -eu -o pipefail
+
+sudo yum install -y gcc gcc-c++ make python3-devel
+
+# Requirements for regression test scripts
+#pip3 install yt matplotlib numpy pandas
+
+# Requirements for building documentation
+#pip3 install sphinx breathe m2r sphinx_rtd_theme linuxdoc sphinx_design sphinx-copybutton
+
+#npm install -g eclint
