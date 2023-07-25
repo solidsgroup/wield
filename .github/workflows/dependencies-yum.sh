08e1cf82f4c4b3e2251f4375715257cb4928c571
Brandon Runnels <brunnels@uccs.edu>
2023-07-25 15:29:27 -0500

    incrmenetal

--- .github/workflows/dependencies-yum.sh
+++ .github/workflows/dependencies-yum.sh
@@ -1,6 +1,6 @@
 set -eu -o pipefail
 
-sudo yum install -y gcc gcc-c++ make python3-devel git
+sudo yum install -y gcc gcc-c++ make python3-devel
 
 # Requirements for regression test scripts
 #pip3 install yt matplotlib numpy pandas
