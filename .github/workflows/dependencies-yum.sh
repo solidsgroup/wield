9d9590edf48358c3f527bc9db0977b9ba425f2d5
Brandon Runnels <brunnels@uccs.edu>
2023-07-25 15:21:14 -0500

    increment

---
+++ .github/workflows/dependencies-yum.sh
@@ -0,0 +1,11 @@
+set -eu -o pipefail
+
+sudo yum install -y g++ make
+
+# Requirements for regression test scripts
+#pip3 install yt matplotlib numpy pandas
+
+# Requirements for building documentation
+#pip3 install sphinx breathe m2r sphinx_rtd_theme linuxdoc sphinx_design sphinx-copybutton
+
+#npm install -g eclint
