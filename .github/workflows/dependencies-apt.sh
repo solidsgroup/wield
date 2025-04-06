6d8185d0e5ea3afec836c92d84c13793f04a6e6f
Brandon Runnels <brunnels@iastate.edu>
2025-04-05 22:46:56 -0500

    adding tclap

--- .github/workflows/dependencies-apt.sh
+++ .github/workflows/dependencies-apt.sh
@@ -1,4 +1,4 @@
 set -eu -o pipefail
 sudo apt update
-sudo apt install -y --no-install-recommends build-essential g++ libpython3-dev libeigen3-dev 
+sudo apt install -y --no-install-recommends build-essential g++ libpython3-dev libeigen3-dev libtclap-dev
 sudo apt install -y python3-numpy python3-matplotlib python3-pybind11 
